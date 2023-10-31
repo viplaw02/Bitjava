2627d41334209139dca411e7a75613669fc9e558
@Bitjava <148614032+viplaw02@users.noreply.github.com>
2023-10-31 15:20:35 +0530

    Update string.c

--- string.c
+++ string.c
@@ -1,28 +1,27 @@
 #include<stdio.h>
 int main(){
-	char name[30];
+	char name1[30];
+	char name2[30];
 	 int i, count1 =0,count2=0;
-	 printf("enter name:");
-	 gets(name);
-	 for(i=0;name[i]!='\0';i++)
+	 printf("enter name1:");
+	 gets(name1);
+	 for(i=0;name1[i]!='\0';i++)
 	 {
 	 	count1++;
-	 	
+
 	 }
-     printf("enter name:");
-	 gets(name);
-	 for(i=0;name[i]!='\0';i++)
+     printf("enter name2:");
+	 gets(name2);
+	 for(i=0;name2[i]!='\0';i++)
 	 {
 	 	count2++;
-	 	
+
 	 }
 	 if(count1==count2){
 	 	printf("your string are equal");
-	 	
+
 	 }
 	 else{
 	 	printf("given string are not equal");
 	 }
 }
-	 
-	
